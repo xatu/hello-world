ab2bd26c24a333ae4dc912eca7c9735cf1a18f42
xatu <rafaelf.mezquita@gmail.com>
2018-03-03 16:00:02 -0400

    Added properties to components

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+//Dependencies
 import React, { Component } from 'react';
 
 //Components
@@ -5,13 +6,16 @@ import Header from './global/Header';
 import Content from './global/Content';
 import Footer from './global/Footer';
 
+//Data
+import items from '../data/menu';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header title="Test" items={items} />
         <Content />
-        <Footer />
+        <Footer license="&copy; Xatu 2017" />
       </div>
     );
   }
--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -1,11 +1,21 @@
+//Dependencies
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+//Assets
 import './css/Footer.css';
 
 class Footer extends Component {
+  static propTypes = {
+    license: PropTypes.string
+  };
+
   render() {
+    const { license } = this.props;
+
     return (
       <div className="Footer">
-        <p>Footer - Copyright..</p>
+        <p>{license}</p>
       </div>
     );
   }
